99f41812c9efea3e83cbdcd42d1d45d1f891dd51
amx100 <amx281@gmail.com>
2023-04-03 15:46:02 +0200

    Setting up filter/search option

--- API/Contracts/IBaseRepository.cs
+++ API/Contracts/IBaseRepository.cs
@@ -6,8 +6,8 @@ namespace Application.Contracts
 {
     public interface IBaseRepository<T>
     {
-        IQueryable<T> FindAll();
-        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        IQueryable<T> SelectAll();
+        IQueryable<T> SelectByCondition(Expression<Func<T, bool>> expression);
         void Create(T entity);
         void Update(T entity);
         void Delete(T entity);
